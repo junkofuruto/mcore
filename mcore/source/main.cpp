e3679313a7d13562f44f06b7dc77d9160eb5abcb
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-07-03 12:33:41 +0300

    datatypes done

--- mcore/source/main.cpp
+++ mcore/source/main.cpp
@@ -3,6 +3,9 @@
 #include "server/protocol/packet.h"
 
 int main(void) {
+	protocol::datatype::f8_t point {0b111, 0b11111};
+
+	shell::post(shell::post_type::Info, std::to_string(static_cast<float>(point.value) + (static_cast<float>(point.fractional) / 32.0)));
 
 	return MC_SHELL_SUCCESS;
 }
--- mcore/source/server/protocol/datatype.h
+++ mcore/source/server/protocol/datatype.h
@@ -9,12 +9,23 @@ namespace protocol {
 			signed char value;
 		} s8_t;
 		typedef struct {
-			unsigned char value : 4;
-			unsigned char fractional : 4;
+			unsigned char value : 3;
+			unsigned char fractional : 5;
 		} f8_t;
 
 		typedef struct {
+			signed short value;
+		} s16_t;
+		typedef struct {
+			unsigned short value : 11;
+			unsigned short fractional : 5;
+		} f16_t;
 
-		} ;
+		typedef struct {
+			char value[1024];
+		} array_t;
+		typedef struct {
+			char value[64];
+		} string_t;
 	}
 }
